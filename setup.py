7183b2703deb5f220e15de02061ebeb92e684ac8
Michele Maestrale <michele.maestrale@glovoapp.com>
2023-12-17 10:04:08 +0100

    first version of hello world library

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='mylibrary',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple print function',
+    long_description=open('README.md').read() if os.path.exists('README.md') else '',
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/mylibrary',
+    install_requires=[
+        # Dependencies here, e.g., 'requests'
+    ],
+)
---
+++ test.py
@@ -0,0 +1,2 @@
+from mylibrary.hello import say_hello
+say_hello()  # This should print "Hello, World!"
