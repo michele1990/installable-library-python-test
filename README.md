20c6b3e22494c28f466f34c8e3c7cabb8a94920e
Michele Maestrale <michele.maestrale@glovoapp.com>
2023-12-17 10:40:56 +0100

    setup update

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # installable-library-python-test
 a test of simple library for python to be pip installed 
+
+
+path:
+
+mylibrary/
+│
+├── mylibrary/
+│   ├── __init__.py
+│   └── hello.py
+│
+└── setup.py
+
+
+mkdir mylibrary
+touch mylibrary/__init__.py
+touch mylibrary/hello.py
+touch setup.py
+
+
+git clone git@github.com-personal:michele1990/installable-library-python-test.git
+
+pip install git+https://github.com/michele1990/installable-library-python-test.git
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name='mylibrary',
+    name='test-lib',
     version='0.1',
     packages=find_packages(),
     description='A simple print function',
@@ -10,7 +10,7 @@ setup(
     long_description_content_type='text/markdown',
     author='Your Name',
     author_email='your.email@example.com',
-    url='https://github.com/yourusername/mylibrary',
+    url='https://github.com/michele1990/installable-library-python-test.git',
     install_requires=[
         # Dependencies here, e.g., 'requests'
     ],
