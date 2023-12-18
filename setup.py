b0d70265a335c85ee221415e207eeeeb2a35545c
Michele Maestrale <michele.maestrale@glovoapp.com>
2023-12-18 09:32:34 +0100

    modifying again lib name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name='testlib',
+    name='mylibrary',
     version='0.1',
     packages=find_packages(),
     description='A simple print function',
